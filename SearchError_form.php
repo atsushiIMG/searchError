a6ab52fd8f88e640e06fcfe77c22abc3b7cf8e97
atsushiIMG <57004449+atsushiIMG@users.noreply.github.com>
2020-01-31 18:26:00 +0900

    Update SearchError_form.php

--- SearchError_form.php
+++ SearchError_form.php
@@ -19,11 +19,3 @@
 	</form>
 </body>
 </html>
-<!-- 
-2019 31
-2017 29
-2015 27 
-Traceback (most recent call last):
-File "2-256_2.py", line 5, in <module>
-IndexError: list assignment index out of range
- -->
